7dd312251d33e5db12fa89ec041ace610edf68f2
48562 <bkmzlitel@gmail.com>
2024-02-16 16:53:57 +0500

    lab3

---
+++ app/src/main/java/ru/bkmz/myapplication/SecondActivity.java
@@ -0,0 +1,30 @@
+package ru.bkmz.myapplication;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second); // Устанавливаем layout
+
+        // Определяем элементы, в данном случае TextView и кнопку
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) TextView textView = findViewById(R.id.textView6);
+        textView.setText("Вы ввели:" + MainActivity.text2remember); // Устанавливаем текст из переменной MainActivity.text2remember
+
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) Button button = findViewById(R.id.button2);
+
+        // Устанавливаем слушатель для кнопки для возврата в MainActivity
+        button.setOnClickListener(v -> {
+            Intent intent = new Intent(SecondActivity.this, MainActivity.class);
+            startActivity(intent);
+        });
+    }
+}
